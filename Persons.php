0230f93d05653be9d68870b03b7119eade1f3d67
(MIS)-Christopher Desiderio <csmdesiderio29@gmail.com>
2021-06-22 17:42:12 +0800

    removed class call isinde try catch block

--- Persons.php
+++ Persons.php
@@ -1,9 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-require "./bootstrap.php";
+require_once "./bootstrap.php";
 
 use Cartrack\Controller\PersonController;
 use Cartrack\Routes\Api as RoutesApi;
@@ -16,7 +12,6 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
 
 $requestMethod = $_SERVER['REQUEST_METHOD'];
-
 if (! in_array($requestMethod, array('GET','POST','PUT','DELETE'))) {
 	Response::methodNotAllowed();
 	return;
@@ -24,7 +19,6 @@ if (! in_array($requestMethod, array('GET','POST','PUT','DELETE'))) {
 
 $headers = getallheaders();
 $jwt = $headers['Authorization'];
-
 if (empty($jwt)) {
 	Response::unauthorized();
 	return;
@@ -34,25 +28,24 @@ try {
 	//validate JWT 
 	$result = JWT::decode($jwt, $_ENV['SECRET_KEY'], array($_ENV['ALGORITHM']));
 	$logId = (int) $result->data[0]->user_id;
-
-	//get id in url
-	$routes = new RoutesApi();
-	$data = $routes->uri();
-	$personId = $data['id'];
-
-	$filterData = array();
-	if (! $personId){
-		$filterService = new FilterService($_GET);
-		$filterData = $filterService->filterData();
-	}
-
-	$personController = new PersonController($requestMethod, $personId, $logId, $filterData);
-	$personController->processRequest();
 } catch(Exception $e) {
 	Response::unauthorized($e->getMessage());
 	return;
 }
 
+//get id in url
+$routes = new RoutesApi();
+$data = $routes->uri();
+$personId = $data['id'];
+
+$filterData = array();
+if (! $personId){
+	$filterService = new FilterService($_GET);
+	$filterData = $filterService->filterData();
+}
+
+$personController = new PersonController($requestMethod, $personId, $logId, $filterData);
+$personController->processRequest();
 
 
 
